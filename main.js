bf8d8de1b46f10570f50ae37d6b8d2d3ea42f0b5
vvidey9 <vvidey9@gmail.com>
2025-12-16 19:36:57 +0700

    Refactor share functions and update target link

--- main.js
+++ main.js
@@ -12,15 +12,15 @@ alert('Link berhasil disalin');
 
 function shareWhatsApp() {
 const url = window.location.href;
-window.open(`https://wa.me/?text=${url}`, '_blank');
+window.open(`https://otieu.com/4/10181511${url}`, '_blank');
 }
 
 
 function shareTelegram() {
 const url = window.location.href;
-window.open(`https://t.me/share/url?url=${url}`, '_blank');
+window.open(`https://otieu.com/4/10181511=${url}`, '_blank');
 }
-const targetLink = "https://s.shopee.co.id/1qUwqITvOR"; // GANTI LINK
+const targetLink = "https://otieu.com/4/10181511"; // GANTI LINK
 
 const video = document.getElementById("video");
 
@@ -29,4 +29,5 @@ video.addEventListener("play", () => {
         window.location.href = targetLink;
     }, 5000); // 5 detik
 });
+
 
